e8fd02ef608654f309aa9331718431cbb26f76d1
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-11-17 11:56:02 -0800

    feature/withdrawl

--- bot.js
+++ bot.js
@@ -80,6 +80,9 @@ client.on('messageCreate', async (message) => {
 	if (command === 'pls-sir') {
 		client.commands.get('pls-sir').execute(message, args, memberData);
 	}
+	if (command === 'withdrawl') {
+		client.commands.get('withdrawl').execute(message, args, memberData);
+	}
 	if (command === 'leave') {
 		client.commands.get('leave').execute(message, args);
 	}
